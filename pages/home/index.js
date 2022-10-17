03a512667e1ecfb437c0b7fb61f2372bdd09786a
Diego <diegohenriqueptc@hotmail.com>
2022-10-17 08:08:35 -0300

    entrega

--- pages/home/index.js
+++ pages/home/index.js
@@ -1,1 +1,45 @@
 /* Desenvolva sua lógica aqui...*/
+function btnDesability(){
+    const user = document.querySelector("#userGit").value;
+
+    if(user){
+        document.querySelector("#send").disabled = false
+        return
+    }else{
+        document.querySelector("#send").disabled = true
+    }
+}
+
+const btn = document.querySelector("#send")
+
+btn.addEventListener("click", function(event){
+    event.preventDefault()
+
+    const name = document.querySelector("#userGit")
+    const value = name.value
+
+    localStorage.setItem("nameUsers", JSON.stringify(value))
+    console.log(value)
+
+    getData(value)
+    window.location = "../../pages/profile/index.html"
+})
+
+async function getData(value){
+
+
+ await fetch(`https://api.github.com/users/${value}/repos`)
+.then(function(response) {return response.json()})
+.then(function(responseJson){
+    
+    responseJson.forEach(element => {
+        
+        console.log(element)
+    });
+
+})
+}
+getData()
+
+
+
--- pages/profile/index.js
+++ pages/profile/index.js
@@ -1,1 +1,69 @@
 /* Desenvolva sua lógica aqui...*/
+let consulta = localStorage.getItem('nameUsers')
+let getUser = JSON.parse(consulta)
+console.log(getUser)
+
+async function getData(value){
+
+
+    await fetch(`https://api.github.com/users/${value}`)
+   .then(function(response) {return response.json()})
+   .then(function(responseJson){
+
+       let avatar = document.querySelector("#avatar")
+       let nameUser = document.querySelector("#nav-name-user")
+       let bio = document.querySelector("#bio-user")
+
+       avatar.src = `${responseJson.avatar_url}`
+       nameUser.innerText = `${responseJson.name}`
+       bio.innerText = `${responseJson.bio}`
+       
+   })
+   }
+getData(getUser)
+
+function backToHomePage(){
+    window.location = "../home/index.html"
+}
+
+ async function getRepoData(value){
+    let ul = document.querySelector(".repository")
+
+    await fetch(`https://api.github.com/users/${value}/repos`)
+   .then(function(response) {return response.json()})
+   .then(function(responseJson){
+
+    console.log(responseJson)
+       responseJson.map(element => {
+        console.log(element)
+
+        let li = document.createElement("li")
+        let div = document.createElement("div")
+        let h2 = document.createElement("h2")
+        let description = document.createElement("p")
+        let divBtn =document.createElement("div")
+        let repos = document.createElement("a")
+        let demo = document.createElement("a")
+
+        li.className = "repos-card"
+        h2.innerText = element.name
+        description.innerText = element.description
+        repos.innerText = "Repositório"
+        repos.href = element.url
+        repos.className ="btn-rep-demo"
+        demo.innerText = "Demo"
+        demo.href = element.homepage
+        demo.className ="btn-rep-demo"
+
+        divBtn.append(repos, demo)
+        div.append(h2, description, divBtn)
+        li.append(div)
+        ul.append(li)
+
+        
+        
+       });
+   
+   })
+}
+getRepoData(getUser)
